75da2f1296bbbe3a17f54446c5ca1adcb5b2add8
Sruthi <sruthiganesan11@gmail.com>
2023-12-17 21:16:17 +0530

    task4

--- guvi-tasks/src/main/java/com/sruthi/task4/StudentManagement.java
+++ guvi-tasks/src/main/java/com/sruthi/task4/StudentManagement.java
@@ -1,14 +1,14 @@
 package com.sruthi.task4;
 
 public class StudentManagement {
-    public static void main(String[] args) throws NameNotValidException,AgeNotWithinRangeException{
+    public static void main(String[] args) throws NameNotValidException, AgeNotWithinRangeException {
 
-       // Student student1 = new Student(101,"John",18,"BCA");
-       // Student student2 = new Student(102,"Jo%hn",18,"BA");
-       Student student3 = new Student(103,"Elena",13,"B.Com");
+        Student student1 = new Student(101, "John", 18, "BCA");
+        Student student2 = new Student(102, "Jo%hn", 18, "BA");
+        Student student3 = new Student(103, "Elena", 13, "B.Com");
 
-       // System.out.println(student1);
-        //System.out.println(student2);
+        System.out.println(student1);
+        System.out.println(student2);
         System.out.println(student3);
     }
 }
