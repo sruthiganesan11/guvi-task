98cbcb9d4a1868ef3be7a7e5b99c43d7ad56b08e
Sruthi <sruthiganesan11@gmail.com>
2023-12-18 12:35:37 +0530

    task-5

--- guvi-tasks/src/main/java/com/sruthi/task5/AgeCalculator.java
+++ guvi-tasks/src/main/java/com/sruthi/task5/AgeCalculator.java
@@ -1,6 +1,8 @@
 package com.sruthi.task5;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class AgeCalculator {
@@ -8,9 +10,25 @@ public class AgeCalculator {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your date of birth dd-mm-yyyy");
+        System.out.println("Enter your date of birth yyyy-mm-dd");
         String dob = scanner.nextLine();
+        LocalDate birthDate = null;
+        try {
+            birthDate = LocalDate.parse(dob);
 
-        LocalDate date;
+        }catch (DateTimeParseException e) {
+            System.out.println("Enter your dob: yyyy-mm-dd");
+            System.exit(-1);
+        }
+        LocalDate currentDate = LocalDate.now();
+        Period age = Period.between(birthDate, currentDate);
+
+        int years = age.getYears();
+        int month = age.getMonths();
+        int days = age.getDays();
+
+        System.out.println(years + " years " + month + " months and " + days + " days");
+
+
     }
 }
